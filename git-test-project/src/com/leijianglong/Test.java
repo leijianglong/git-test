abbeffb6615c79fbf904dc23a04a158878e2c6e4
leijijanglong <1743997748@qq.com>
2019-05-05 18:17:14 +0800

    提交代码噢

---
+++ git-test-project/src/com/leijianglong/Test.java
@@ -0,0 +1,20 @@
+package com.leijianglong;
+
+/**
+ * @Description TODO
+ * @Author leijianglong
+ * @Date 2019/5/5 17:50
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+    }
+
+}
